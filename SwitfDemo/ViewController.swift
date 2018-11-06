c02b0ed5b09c03650c193497573a3c6dfb9fcc68
JXS <h229315523@163.com>
2018-11-06 16:29:09 +0800

    Initial Commit

---
+++ SwitfDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwitfDemo
+//
+//  Created by mtzn on 2018/10/30.
+//  Copyright © 2018 HCQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+}
+
---
+++ SwitfDemoTests/SwitfDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwitfDemoTests.swift
+//  SwitfDemoTests
+//
+//  Created by mtzn on 2018/10/30.
+//  Copyright © 2018 HCQ. All rights reserved.
+//
+
+import XCTest
+@testable import SwitfDemo
+
+class SwitfDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
